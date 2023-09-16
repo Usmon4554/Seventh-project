7222846a6ff5cdfed072b12a9d1b50ec16bbdcf1
Usmon4554 <usmontoraxon5@gmail.com>
2023-09-16 21:54:55 +0300

    commit

---
+++ js/swiper.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: 40,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+  });
